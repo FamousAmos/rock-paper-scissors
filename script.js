831737c2aedf215f028607d01cfe8e965c3626b0
Amos Kenner <akenner92@gmail.com>
2022-05-31 16:31:31 -0500

    Added score board

--- script.js
+++ script.js
@@ -3,49 +3,66 @@ let playerScore = 0;
 let computerScore = 0;
 const options = ["Rock", "Paper", "Scissors"];
 let buttons = document.querySelectorAll(".button");
+const results = document.querySelector(".results");
+const playerScoreBox = document.querySelector("#player-score");
+const computerScoreBox = document.querySelector("#computer-score");
+const retry = document.querySelector(".retry");
 
 function computerPlay() {
   const selection = Math.floor(Math.random() * 100) % 3;
   return options[selection];
 }
 
-function capitalize(str) {
-  const lower = str.toLowerCase();
-  return str.charAt(0).toUpperCase() + lower.slice(1);
-}
-
 function playRound(playerSelection, computerSelection) {
-  playerSelection = capitalize(playerSelection);
-
   if (playerSelection == computerSelection) {
-    return `It's a tie! You both chose ${playerSelection}.`;
+    results.textContent = `It's a tie! You both chose ${playerSelection}.`;
   } else if (
     (playerSelection == "Rock" && computerSelection == "Scissors") ||
     (playerSelection == "Paper" && computerSelection == "Rock") ||
     (playerSelection == "Scissors" && computerSelection == "Paper")
   ) {
     playerScore += 1;
-    return `You win! ${playerSelection} beats ${computerSelection}.`;
+    playerScoreBox.textContent = playerScore;
+    results.textContent = `You win! ${playerSelection} beats ${computerSelection}.`;
   } else {
     computerScore += 1;
-    return `You lose! ${playerSelection} loses to ${computerSelection}.`;
+    computerScoreBox.textContent = computerScore;
+    results.textContent = `You lose! ${playerSelection} loses to ${computerSelection}.`;
+  }
+
+  if (playerScore == 5 || computerScore == 5) {
+    checkWinner();
   }
 }
 
 function checkWinner() {
   if (playerScore > computerScore) {
-    return `You beat the computer ${playerScore} to ${computerScore}!`;
+    results.textContent = `You beat the computer!`;
   } else if (computerScore > playerScore) {
-    return `You lose to the computer ${computerScore} to ${playerScore}!`;
-  } else {
-    return `No winner this game! You both scored ${playerScore}`;
+    results.textContent = `You lost to the computer!`;
   }
+  retry.classList.remove("hide");
+}
+
+function reset() {
+  playerScore = 0;
+  computerScore = 0;
+  playerScoreBox.textContent = playerScore;
+  computerScoreBox.textContent = computerScore;
+  results.textContent = "";
+  retry.classList.add("hide");
 }
 
 buttons.forEach((button) => {
   button.addEventListener("click", () => {
     const selection = button.querySelector("img");
     playerSelection = selection.alt;
-    console.log(playerSelection);
+    if (playerScore < 5 && computerScore < 5) {
+      playRound(playerSelection, computerPlay());
+    }
   });
+});
+
+retry.addEventListener("click", () => {
+  reset();
 });
